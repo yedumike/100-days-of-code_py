6fcfaa1d935a6c5419847ac8b05261f401c7dc2a
yedumike <yedumike@gmail.com>
2024-02-17 11:08:36 +0000

    finding factors of a number

---
+++ day7.py
@@ -0,0 +1,9 @@
+#FINDING FACTORS OF A NUMBER
+while True:
+    num = int(input("Enter a num: "))
+    fac = [num]
+    for i in range (1,num):
+        if num % i == 0:
+            fac.append(i)
+    fac.sort()
+    print(fac)
