591e579e332cb5d3111abeee5a97565093a8e2ce
yedumike <yedumike@gmail.com>
2024-10-12 08:31:11 +0000

    Countdown Timer

---
+++ day10.py
@@ -0,0 +1,14 @@
+#COUNTDOWN Timer
+
+import time
+
+my_time = int(input("For how long: "))
+
+for i in range(my_time,0,-1):
+    seconds = i % 60
+    minutes = int(i / 60) % 60
+    hours = int(i / 3600) #may add %24 when including days in f string -- next line
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("Time is up!")
