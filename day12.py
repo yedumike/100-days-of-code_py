588dc35cebd2fa7d9ee807a0ebd6cf7b2c9e6c75
yedumike <yedumike@gmail.com>
2024-11-13 13:50:23 +0000

    password generator

--- day12.py
+++ day12.py
@@ -1,3 +1,5 @@
+#Password Generator
+
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers = ["0","1","2","3","4","5","6","7","8","9"]
