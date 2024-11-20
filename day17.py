55c4c0ff195ad016c5b7ea71df891cf16626ebdb
yedumike <yedumike@gmail.com>
2024-11-20 23:29:14 +0000

    Number Guessing game

--- day17.py
+++ day17.py
@@ -1,5 +1,7 @@
 from random import randint
 
+#Number guessing game
+
 EASY_LEVEL_TURNS =  10
 HARD_LEVEL_TURNS = 5
 
@@ -39,12 +41,13 @@ def game():
     turns = set_difficulty()
 
     guess = 0
-    while guess != answer:
+    while guess != answer and turns != 0:
         print(f"You have {turns} attempts remaining to guess the number")
         guess = int(input("Make a guess: "))
         turns = check_answer(guess,answer,turns)
+        if turns == 0:
+            print("You don't have remaining attempts")
     # Track the number of turns and reduce by 1 if they get it wrong
-
     # Repeat the guessing functionality if they get it wrong
 
 game()
